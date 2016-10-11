06c5b067960303d37a8dcb17d1fe559fcfe7373a
Sergi Maymi <smaymi@portic.net>
2016-10-11 16:37:24 +0200

    added location

--- Models/Player.cs
+++ Models/Player.cs
@@ -6,7 +6,8 @@ namespace webapi.Models
 
     public class Player
     {
-
+        public double longitude { get; set; }
+        public double latitude { get; set; }
         public string username { get; set; }
         public DateTime joinedAt { get; set; }
         public List<Movement> movements { get; set; }
